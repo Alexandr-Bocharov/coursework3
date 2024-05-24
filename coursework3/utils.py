703e21444726dfb546d26d5fe0006b46390d92bd
tanjiro <counter23062001@mail.ru>
2024-05-24 19:06:33 +0300

    combine get_valid_receiver and get_valid_sender funcs to get_valid_card_info

--- coursework3/utils.py
+++ coursework3/utils.py
@@ -6,7 +6,7 @@ def operations_reader() -> list:
     '''
     Читаем и возвращаем содержимое файла "operations.json"
     '''
-    with open('operations.json', 'r') as file:
+    with open('/Users/aleksandrbocarov/coursework3/coursework3/operations.json', 'r') as file:
         return json.load(file)
 
 
@@ -29,15 +29,15 @@ def get_date(date: str) -> str:
     return dt.strftime('%d.%m.%Y')
 
 
-def get_valid_sender(sender: str) -> str:
+def get_valid_card_info(info: str) -> str:
     '''
     Возвращаем информацию по карте/счету-отправителю в необходимом формате
     '''
-    if 'Счет' in sender:
-        sender = sender.replace(sender[-20: -4], 2 * '*')
+    if 'Счет' in info:
+        sender = info.replace(info[-20: -4], 2 * '*')
         return sender
     else:
-        splitter_by_space = sender.split()
+        splitter_by_space = info.split()
         first_four = splitter_by_space[-1][-16:-12]
         second_four = splitter_by_space[-1][-12:-8]
         third_four = splitter_by_space[-1][-8: -4]
@@ -51,26 +51,24 @@ def get_valid_sender(sender: str) -> str:
 
 
 
-def get_valid_receiver(receiver: str) -> str:
-    '''
-    Возвращаем информацию по карте/счету-получателю в необходимом формате
-    '''
-    if 'Счет' in receiver:
-        receiver = receiver.replace(receiver[-20: -4], 2 * '*')
-        return receiver
-    else:
-        splitter_by_space = receiver.split()
-        first_four = splitter_by_space[-1][-16:-12]
-        second_four = splitter_by_space[-1][-12:-8]
-        third_four = splitter_by_space[-1][-8: -4]
-        four_four = splitter_by_space[-1][-4:]
-        new_splitter = splitter_by_space[:-1]
-        new_splitter.extend([first_four,
-                            second_four.replace(second_four[-2:], '**'),
-                            third_four.replace(third_four, '****'),
-                            four_four])
-        return ' '.join(new_splitter)
-
-
-
-
+# def get_valid_receiver(receiver: str) -> str:
+#     '''
+#     Возвращаем информацию по карте/счету-получателю в необходимом формате
+#     '''
+#     if 'Счет' in receiver:
+#         receiver = receiver.replace(receiver[-20: -4], 2 * '*')
+#         return receiver
+#     else:
+#         splitter_by_space = receiver.split()
+#         first_four = splitter_by_space[-1][-16:-12]
+#         second_four = splitter_by_space[-1][-12:-8]
+#         third_four = splitter_by_space[-1][-8: -4]
+#         four_four = splitter_by_space[-1][-4:]
+#         new_splitter = splitter_by_space[:-1]
+#         new_splitter.extend([first_four,
+#                             second_four.replace(second_four[-2:], '**'),
+#                             third_four.replace(third_four, '****'),
+#                             four_four])
+#         return ' '.join(new_splitter)
+for line in get_last_executed_operations(operations_reader()):
+    print(line)
